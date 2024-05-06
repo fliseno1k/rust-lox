ac2b6b88132e24fe891684d6d67f8f86f8272788
fliseno1k <fliseno1k@gmail.com>
2024-05-06 07:21:33 +0300

    initial

---
+++ crates/lox/main.rs
@@ -0,0 +1,15 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 1 {
+        eprintln!("Usage: lox [path]");
+        return;
+    }
+
+    let path = args.first().unwrap();
+    let data = fs::read_to_string(path).unwrap();
+
+    println!("{}", data);
+}
