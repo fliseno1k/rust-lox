774e1f40cc514f9101b13638fa5dd50286d70fe6
fliseno1k <fliseno1k@gmail.com>
2024-07-07 17:30:47 +0300

    feat(syntax:parser): implement parser

--- crates/lox-syntax/src/lib.rs
+++ crates/lox-syntax/src/lib.rs
@@ -1,4 +1,14 @@
 pub mod position;
 
+#[macro_use]
+mod parser;
+mod common;
 mod token;
 mod tokenizer;
+
+pub fn parse(code: &str) {
+    use tokenizer::tokenize_with_context;
+
+    let tokens = tokenize_with_context(code);
+    let mut parser = crate::parser::Parser::new(&tokens);
+}
